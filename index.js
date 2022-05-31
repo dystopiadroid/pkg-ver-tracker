82bd0af4742c44914b52ef19c7a395a4f7e95945
Balaji PJ <bpjdev25@gmail.com>
2022-05-31 11:12:07 +0530

    + Fetched package name and package details from cmd args + Handled errors regarding cmd args

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 function main() { 
+  if (!(process.argv.indexOf("-i")+1)) 
+  { 
+    console.log("Usage: node . -i <CSV-file> <depName@version>"); 
+    return;
+  }
+  const [fileName, pkgDetails] = (process.argv.splice(process.argv.indexOf("-i") + 1))
+  const [pkgName, pkgVersion] = pkgDetails.split("@");
 
 }
 
